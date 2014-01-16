017ffbcc73f0e34578a717d596d4e22ba98ac633
Saravanan Vijayakumaran <sarva.v@gmail.com>
2014-01-16 12:32:55 +0530

    Removing Alt-l key binding to use it Latex-Suite

---
+++ xmonad.hs
@@ -0,0 +1,22 @@
+-- default desktop configuration for Fedora
+
+import System.Posix.Env (getEnv)
+import Data.Maybe (maybe)
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Config.Kde
+import XMonad.Config.Xfce
+
+import XMonad.Util.EZConfig
+
+main = do
+     session <- getEnv "DESKTOP_SESSION"
+     xmonad  $ removeKeysP (maybe desktopConfig desktop session) ["M-l"]
+
+desktop "gnome" = gnomeConfig
+desktop "kde" = kde4Config
+desktop "xfce" = xfceConfig
+desktop "xmonad-mate" = gnomeConfig
+desktop _ = desktopConfig
