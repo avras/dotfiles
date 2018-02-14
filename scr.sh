f9e15bdb8f7edccacb82da2c83f16b3855e01eac
Saravanan Vijayakumaran <sarva.v@gmail.com>
2018-02-14 16:19:08 +0530

    Added screen bash file

---
+++ scr.sh
@@ -0,0 +1,6 @@
+xrandr --output DisplayPort-2 --rotate left
+xrandr --output DisplayPort-2 --mode 1920x1200
+xrandr --output DVI-0 --mode 1920x1080
+xrandr --output DVI-0 --primary
+xrandr --output DVI-0 --pos 0x0 --output DisplayPort-2 --pos 1920x0
+
